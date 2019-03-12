6885b70bb7f14328ed26dac0920cb1298ef3737b
Francisco Souza <Franciscosouza92@gmail.com>
2019-03-12 19:10:08 +0100

    Updated to Typescript + Installed React-Router if needed. Also installed Sass.

---
+++ src/components/Header.tsx
@@ -0,0 +1,13 @@
+import * as React from "react";
+
+class Header extends React.Component {
+  render() {
+    return (
+      <header>
+        <div className="grid-16"></div>
+      </header>
+    );
+  }
+}
+
+export default Header;
